6009b55cc3ac2d102f94c96e2f5d1809025ea698
Christopher Rzepka <christopher.rzepka@sympatico.ca>
2023-01-01 18:13:55 -0500

    Add project files.

---
+++ CSharp-Console-Starter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharp_Console_Starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            DateTime currentDate = DateTime.Now;
+
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
